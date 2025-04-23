13186b65c6e2c0e14011ce28c278c08f7bdb865f
Aisyahkeifaxa <studyfileaisyahkeifa@gmail.com>
2025-04-23 15:25:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 KEIFAKEREN")
 st.write(
     "Hello welcome to my world [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-St. image("IMG_20250421_152014.jpg") 
+St.image("IMG_20250421_152014.jpg") 
