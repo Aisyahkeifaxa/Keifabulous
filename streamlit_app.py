3fcdcc4f4ffd4b4dfa695fc837100a236d983285
Aisyahkeifaxa <studyfileaisyahkeifa@gmail.com>
2025-04-23 14:55:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 KEIFAKEREN")
 st.write(
-    "Hello welcome to my world"[docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hello welcome to my world [docs.streamlit.io](https://docs.streamlit.io/)."
 )
