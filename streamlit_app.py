7e7dfd6e98d6e0d4675e7ab7f517e7741cf231c5
Aisyahkeifaxa <studyfileaisyahkeifa@gmail.com>
2025-04-23 14:50:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 KEIFAKEREN")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hello welcome to my world"[docs.streamlit.io](https://docs.streamlit.io/)."
 )
